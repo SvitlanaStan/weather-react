e934c2cd847510e04960223fe28bc4bda7783ba7
SvitlanaStan <118539952+SvitlanaStan@users.noreply.github.com>
2023-03-21 15:00:45 +0200

    body moved to weather.js

--- src/App.js
+++ src/App.js
@@ -1,85 +1,22 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Weather from "./Weather.js";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
-        <div class="weather-app">
-          <h1>New York, USA</h1>
-          <h2>
-            <span id="temperature"> +18 </span>
-            <span id="tempType">
-              <a href="#" id="celsius-link" class="active">
-                {" "}
-                °C{" "}
-              </a>
-              |
-              <a href="#" id="fahrenheit-link">
-                °F{" "}
-              </a>
-            </span>
-            <img
-              class="box"
-              src=""
-              alt="Clear"
-              id="icon"
-              className="float-left"
-            />
-          </h2>
-          <div class="container text-center">
-            <div class="row align-items-start">
-              <div class="col">
-                <form id="search-form" class="row g-2">
-                  <div class="col-7">
-                    <input
-                      type="search"
-                      placeholder="Type a city"
-                      class="form-control"
-                      id="city-input"
-                      autocomplete="off"
-                    />
-                  </div>
-                  <div class="col">
-                    <button type="submit" class="btn btn-info CheckTemperarute">
-                      Search
-                    </button>
-                  </div>
-                </form>
-              </div>
-              <div class="col">
-                <ul class="current-weather">
-                  <li id="description">Clear</li>
-                  <li>
-                    Humidity <span id="humidity"> 10 </span> %
-                  </li>
-                  <li>
-                    Wind <span id="wind"> 10 </span> km/h
-                  </li>
-                </ul>
-              </div>
-              <ul class="current-data">
-                <li id="date">Wednesday</li>
-                <li id="date">March 21, 2023</li>
-              </ul>
-            </div>
-          </div>
-          <h2>Next 5 Day Weather</h2>
-          <div>
-            <div class="weather-forecast" id="forecast"></div>
-          </div>
-          <br />
-          <small>
-            <a
-              href="https://github.com/SvitlanaStan/weather-react.git"
-              target="_blank"
-              id="githubLink"
-            >
-              Open-source code, by Svitlana Stanislavchuk
-            </a>
-          </small>
-        </div>
+        <Weather defaultCity="New York" />
+
+        <footer>
+          <a
+            href="https://github.com/SvitlanaStan/weather-react.git"
+            target="_blank"
+            id="githubLink"
+          >
+            Open-source code, by Svitlana Stanislavchuk
+          </a>
+        </footer>
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -2,5 +2,74 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
-  return <div className="Weather">Hello Weather</div>;
+  return (
+    <div className="Weather">
+      {" "}
+      <div class="weather-app">
+        <h1>New York, USA</h1>
+        <h2>
+          <span id="temperature"> +18 </span>
+          <span id="tempType">
+            <a href="#" id="celsius-link" class="active">
+              {" "}
+              °C{" "}
+            </a>
+            |
+            <a href="#" id="fahrenheit-link">
+              °F{" "}
+            </a>
+          </span>
+          <img
+            class="box"
+            src=""
+            alt="Clear"
+            id="icon"
+            className="float-left"
+          />
+        </h2>
+        <div class="container text-center">
+          <div class="row align-items-start">
+            <div class="col">
+              <form id="search-form" class="row g-2">
+                <div class="col-7">
+                  <input
+                    type="search"
+                    placeholder="Type a city"
+                    class="form-control"
+                    id="city-input"
+                    autocomplete="off"
+                  />
+                </div>
+                <div class="col">
+                  <button type="submit" class="btn btn-info CheckTemperarute">
+                    Search
+                  </button>
+                </div>
+              </form>
+            </div>
+            <div class="col">
+              <ul class="current-weather">
+                <li id="description">Clear</li>
+                <li>
+                  Humidity <span id="humidity"> 10 </span> %
+                </li>
+                <li>
+                  Wind <span id="wind"> 10 </span> km/h
+                </li>
+              </ul>
+            </div>
+            <ul class="current-data">
+              <li id="date">Wednesday</li>
+              <li id="date">March 21, 2023</li>
+            </ul>
+          </div>
+        </div>
+        <h2>Next 5 Day Weather</h2>
+        <div>
+          <div class="weather-forecast" id="forecast"></div>
+        </div>
+      </div>
+      ;
+    </div>
+  );
 }
